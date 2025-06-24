10cc2f89cb4b1f6dc17c29cb1382c63ffac65513
Nathaniel Prazeres <nathanprazeres@gmail.com>
2025-06-24 22:06:31 +0200

    build: change the minimum sdk version to 26 (android 8.0)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.nathanprazeres.walkunlock"
-        minSdk = 33
+        minSdk = 26
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
